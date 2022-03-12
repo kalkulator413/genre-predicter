41b90dff0e8db3a0819177e115b0e978b5e96bcd
Kalkulator413 <kalkin413@gmail.com>
2022-03-12 10:18:20 -0800

    added graphics

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import torch.nn as nn
 import numpy as np
 import pickle
 
+import cv2
+
+import urllib
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
@@ -19,6 +22,9 @@ with open('model', 'rb') as f:
 def get_genre(name, artist):
   results = sp.search(q="track:" + name + " artist:" + artist, type="track")
   track_id = results['tracks']['items'][0]['id']
+  artist = results['tracks']['items'][0]['artists'][0]['name']
+  name = results['tracks']['items'][0]['name']
+  img_link = results['tracks']['items'][0]['album']['images'][0]['url']
 
   f = sp.audio_features(track_id)[0]
   song = np.array([
@@ -37,7 +43,7 @@ def get_genre(name, artist):
   t = model(song)
   value = (t == max(t)).nonzero(as_tuple=True)[0].detach()
 
-  to_return = (f'{name} by {artist} is {genres[value]} ({round(int(10000*max(t)))/100}% confidence)\n')
+  to_return = (f'Primary genre: {genres[value]} ({round(int(10000*max(t)))/100}% confidence)\n')
   
   if max(t) < .6:
     t_list = list(t)
@@ -50,8 +56,48 @@ def get_genre(name, artist):
     if secondary_confidence > .5:
       to_return += f'Secondary genre: {secondary_genre} ({round(int(10000*secondary_confidence))/100}% confidence)\n'
 
-  return to_return
+  return to_return, artist, name, img_link
+
+def graphics():
+  width = 600
+  if len(text) == 3:
+    height = 230
+  else:
+    height = 200
+  def start():
+    img = 40 * np.ones((height, width, 3), dtype = np.uint8)
+
+    text_size = 2
+    if len(song) > 12:
+      text_size = 24 / len(song)
+    cv2.putText(img, song, (int(width/4), 70), cv2.FONT_HERSHEY_SIMPLEX, text_size, (255, 255, 255), 1, cv2.LINE_AA)
+    text_size = 1
+    if len(artist) > 22:
+      text_size = 22 / len(artist)
+    cv2.putText(img, artist, (int(width/4) + 6, 120), cv2.FONT_HERSHEY_SIMPLEX, text_size, (255, 255, 255), 1, cv2.LINE_AA)
+
+    counter = 0
+    for line in text:
+      cv2.putText(img, line, (20, 166 + 30*counter), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+      counter += 1
+
+    cv2.rectangle(img, (18, 18), (131, 131), (0, 0, 0), 2)
+
+    req = urllib.request.urlopen(link)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    pic = cv2.imdecode(arr, -1) # 'Load it as it is'
+    pic = cv2.resize(pic, (110,110), interpolation = cv2.INTER_AREA)
+
+    img[20:130,20:130] = pic[0:110,0:110]
+
+    return img
+
+  img = start()
+  cv2.imshow('',img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
   
+
 while 1:
     print('>>> Enter song:')
     song = input()
@@ -60,7 +106,10 @@ while 1:
     artist = input()
 
     try:
-        print(get_genre(song, artist))
+      text, artist, song, link = get_genre(song, artist)
+      text = text.split('\n')
+      graphics()
     except Exception as e:
         print('Could not find the song!')
+        raise Exception('Song Not Found')
         # print(e)
